4353acc85b624b3be9148b4dcdd34bf12a28b225
JoeBell <JoeBell@users.noreply.github.com>
2019-04-27 15:59:09 +0100

    ui

---
+++ LudumDare/Assets/Scenes/Joe/MenuScene/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public Panel menu;
+    public Panel control;
+
+    public void startGame()//load the main game
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void quit() // allow the user to quit the game
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void controls() // open the controls
+    {
+        menu.SetActive = false;
+        control.SetActive = true;
+    }
+
+    public void returnMenu()// open the menu
+    {
+        menu.SetActive = true;
+        control.SetActive = false;
+    }
+
+}
---
+++ LudumDare/Assets/Scenes/Joe/ShootingScene/Scripts/OBJpool.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OBJpool : MonoBehaviour
+{
+    //singleton
+    public static OBJpool objectInstance;
+
+    public List<GameObject> thePool;
+
+    public GameObject itemToPool;
+
+    public int amountToPool;
+
+
+
+    private void Awake()
+    {
+        objectInstance = this;
+    }
+
+
+    void Start()
+    {
+        thePool = new List<GameObject>();
+        for (int i = 0; i < amountToPool; i++)
+        {
+            GameObject tempOBJ = Instantiate(itemToPool) as GameObject;
+            tempOBJ.SetActive(false);
+            thePool.Add(tempOBJ);
+        }
+    }
+
+
+    public GameObject GetPooledOBJ()
+    {
+        for (int i = 0; i < thePool.Count; i++)
+        {
+            if (!thePool[i].activeInHierarchy)
+            {
+                return thePool[i];
+            }
+
+        }
+        return null;
+    }
+}
