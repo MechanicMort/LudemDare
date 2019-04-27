726628c69a481951f4a9c5d2c2ffdd9418c71f22
MechanicMort <47052071+MechanicMort@users.noreply.github.com>
2019-04-27 11:48:02 +0100

    CamGayLOL

--- LudumDare/Assets/Scripts/Classes/BaseAttack.cs
+++ LudumDare/Assets/Scripts/Classes/BaseAttack.cs
@@ -5,5 +5,13 @@ using UnityEngine;
 [System.Serializable]
 public class BaseAttack : MonoBehaviour {
 
-	
+    public string AttackName;
+
+    public float Damage;
+    public float Cost;
+
+    public GameObject myProjectile;
+    public Sprite UIElement;
+
 }
+                        
