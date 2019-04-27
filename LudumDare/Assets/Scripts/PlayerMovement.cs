1c80ad264473842a37efa55567da0c44de5274c3
Alex Smith <50043899+AlexSmith292@users.noreply.github.com>
2019-04-27 13:51:38 +0100

    Scene

--- LudumDare/Assets/Scripts/PlayerMovement.cs
+++ LudumDare/Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovemen : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     //private float xMove;
     //private float yMove;
