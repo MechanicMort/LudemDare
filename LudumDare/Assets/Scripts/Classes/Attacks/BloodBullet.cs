dc8076cb1272e3a95ed15ed92ac018eec82655b7
MechanicMort <47052071+MechanicMort@users.noreply.github.com>
2019-04-27 11:51:23 +0100

    commies

---
+++ LudumDare/Assets/Scripts/Classes/Attacks/BloodBullet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodBullet : BaseAttack {
+
+	public BloodBullet ()
+    {
+        AttackName = "Blood Bullet";
+        Damage = 5;
+        Cost = 1;
+    }
+}
